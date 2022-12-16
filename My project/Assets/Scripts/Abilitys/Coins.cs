bb2847bc1dfde5c14ed7b10f4ab193534ea61664
Zakory Stanger <zakory.stangerr@gmail.com>
2022-12-16 11:50:00 -0800

    Coins

---
+++ My project/Assets/Scripts/Abilitys/Coins.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Coins : MonoBehaviour
+{
+    public int coins;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            coins ++;
+            Destroy(gameObject);
+        }
+    }
+}
